210dad762f1c4a5e989b2d7f908448fd165df45b
md-habibullah-99 <habibullahhisam@gmail.com>
2026-01-03 21:27:52 +0600

    - Replaced the existing header and theme toggle in the Home component with HeroHeader and HeroSection. - Added HeroHeader component for navigation and branding. - Introduced HeroSection component for showcasing key features and calls to action. - Updated components.json to include a new registry for @tailark. - Created new Logo component for branding. - Implemented AnimatedGroup and TextEffect components for enhanced animations. - Added motion library for animation effects. - Updated package.json and pnpm-lock.yaml to include motion dependency.

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
+import { HeroHeader } from "@/components/header";
+import HeroSection from "@/components/hero-section";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello world</h1>
-      <ThemeToggle />
+      <HeroHeader />
+      <HeroSection />
     </div>
   );
 }
