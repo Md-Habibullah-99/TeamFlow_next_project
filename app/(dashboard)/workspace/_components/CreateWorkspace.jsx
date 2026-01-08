0107e41092eb1e14c159d29217ba442ff8b4e5ad
md-habibullah-99 <habibullahhisam@gmail.com>
2026-01-08 22:52:54 +0600

    feat: implement workspace management components including creation and user navigation.

---
+++ app/(dashboard)/workspace/_components/CreateWorkspace.jsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import { Plus } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { WorkspaceSchema } from "@/app/schemas/workspace";
+
+
+export default function CreateWorkspace() {
+  const [open, setOpen] = useState(false);
+
+  const form = useForm({
+    resolver: zodResolver(WorkspaceSchema),
+    defaultValues: {
+      name: "",
+    }
+  });
+
+  function onSubmit() {
+    console.log("Data")
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <DialogTrigger asChild>
+            <Button variant="ghost" size="icon" className="size-12 rounded-xl border-2 border-dashed border-muted-foreground/50 text-muted-foreground hover:border-muted-foreground hover:text-foreground hover:rounded-lg transition-all duration-200" >
+              <Plus className="size-5" />
+            </Button>
+          </DialogTrigger>
+        </TooltipTrigger>
+        <TooltipContent side="right">
+          <p>Create Workspace</p>
+        </TooltipContent>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Create Workspace</DialogTitle>
+            <DialogDescription>Create a new workspace to get started</DialogDescription>
+          </DialogHeader>
+          <Form {...form}>
+            <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField control={form.control} name="name" render={({field}) => (
+                <FormItem>
+                  <FormLabel>
+                    <FormControl>
+                      <Input placeholder="My workspace" {...field} />
+                    </FormControl>
+                  </FormLabel>
+                  <FormMessage />
+                </FormItem>
+              )} />
+
+              <Button type="submit" className="mt-4 w-full">Create Workspace</Button>
+            </form>
+          </Form>
+        </DialogContent>
+      </Tooltip>
+    </Dialog>
+  )
+}
